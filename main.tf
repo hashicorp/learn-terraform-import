0d9abd4a67ad07261edfe7291bbd3190f34adee1
franco-camba <franco.camba@hashicorp.com>
2022-03-04 11:03:52 +0000

    updated docker provider version

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.11"
+      version = "~> 2.16"
     }
   }
   required_version = ">= 0.14"
