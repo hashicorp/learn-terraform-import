850a0a0e533219f4fadd9e03d687a7d45d6682b3
Robin Norwood <rnorwood@hashicorp.com>
2022-06-30 14:16:30 -0500

    Update to use unix socket instead of npipe (no longer supported)

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# Configure docker provider
-#
-# https://www.terraform.io/docs/providers/docker/index.html
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.16"
-    }
-  }
-  required_version = ">= 0.14"
-}
-
 provider "docker" {
-  host    = "npipe:////.//pipe//docker_engine"
+  host = "unix:///var/run/docker.sock"
 }
